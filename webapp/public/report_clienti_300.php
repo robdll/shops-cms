47e346916ef2531d584d74cbbc223e4ed417dabf
Roberto Di Lillo <dilillo.roberto@gmail.com>
2025-07-10 11:38:25 -0300

    update report 300

--- webapp/public/report_clienti_300.php
+++ webapp/public/report_clienti_300.php
@@ -1,25 +1,39 @@
 <?php
 session_start();
-
 include('../includes/check-auth.php');
 include('../includes/check-gestore.php');
 include('../includes/db.php');
 
-$result = pg_query($conn, "SELECT * FROM clienti_con_piu_di_300_punti ORDER BY saldo_punti DESC");
+$res = pg_query($conn,
+    'SELECT tessera_id, saldo_punti, nome, cognome, codice_fiscale
+     FROM "Kalunga".clienti_con_piu_di_300_punti
+     ORDER BY saldo_punti DESC');
 ?>
+<?php include('header.php') ?>
 
 <h2>Clienti con più di 300 punti</h2>
-<table border="1">
-    <tr><th>Tessera</th><th>Saldo punti</th><th>Data rilascio</th><th>Cliente</th><th>Codice fiscale</th></tr>
-    <?php while ($row = pg_fetch_assoc($result)) { ?>
-        <tr>
-            <td><?php echo htmlspecialchars($row['tessera_id']) ?></td>
-            <td><?php echo htmlspecialchars($row['saldo_punti']) ?></td>
-            <td><?php echo htmlspecialchars($row['data_rilascio']) ?></td>
-            <td><?php echo htmlspecialchars($row['nome'] . ' ' . $row['cognome']) ?></td>
-            <td><?php echo htmlspecialchars($row['codice_fiscale']) ?></td>
-        </tr>
+
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th>ID Tessera</th>
+      <th>Saldo Punti</th>
+      <th>Nome</th>
+      <th>Cognome</th>
+      <th>Codice Fiscale</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while ($r = pg_fetch_assoc($res)) { ?>
+      <tr>
+        <td><?= htmlspecialchars($r['tessera_id']) ?></td>
+        <td><?= htmlspecialchars($r['saldo_punti']) ?></td>
+        <td><?= htmlspecialchars($r['nome']) ?></td>
+        <td><?= htmlspecialchars($r['cognome']) ?></td>
+        <td><?= htmlspecialchars($r['codice_fiscale']) ?></td>
+      </tr>
     <?php } ?>
+  </tbody>
 </table>
 
-<p><a href="report.php">Torna ai report</a></p>
+<?php include('footer.php') ?>
